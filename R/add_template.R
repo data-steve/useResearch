7ec45d570ff516c6841c3cb0d4b4a063bc71912a
Steve Simpson <steventroysimpson@gmail.com>
2016-04-21 16:31:35 -0400

    updated add_template()

--- R/add_template.R
+++ R/add_template.R
@@ -3,13 +3,17 @@
 #' Copies Template for User Research Process.
 #'
 #' @param rdir the directory to copy the template to
+#' @param open logical var to indicate whether to open file for editing
 #' @export
 #' @examples
 #' \dontrun{
 #' add_template()
 #' }
-add_template <- function(rdir = "./R") {
+add_template <- function(rdir = "./R", open = TRUE) {
   file.copy(
     system.file("templates/zzz_useResearch.R", package = "useResearch")
     , rdir)
+  if (open) file.edit(
+  	file.path(rdir,'zzz_useResearch.R')
+  	)
 }
